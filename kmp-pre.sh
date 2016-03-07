af8916e5bebe36bb4d09dabe721b708902b66f39
Stefan Dirsch <sndirsch@suse.com>
2016-03-07 03:07:04 -0800

    - kmp-pre.sh: * get rid of old kernel modules and weak-updates(2) symlinks (bnc#969763)

--- kmp-pre.sh
+++ kmp-pre.sh
@@ -1,3 +1,8 @@
+# get rid of old kernel modules and weak-updates(2) symlinks (bnc#969763)
+flavor=%1
+rm -f /lib/modules/*-$flavor/updates/nvidia*.ko
+find  /lib/modules/*-$flavor/weak-updates -name "nvidia*.ko" -delete || true
+
 if [ -x /usr/bin/nvidia-uninstall ]; then
     /usr/bin/nvidia-uninstall -s || :
 fi;
