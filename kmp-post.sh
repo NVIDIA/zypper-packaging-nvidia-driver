f32f7780360150a42063623c7990f94fceae4a7c
Stefan Dirsch <sndirsch@suse.com>
2016-09-30 06:12:19 -0700

    - %post: create symlinks and /etc/tmpfiles.d snippet for udev so nvidia devices will get user ACLs by logind later (bnc#1000625)

--- kmp-post.sh
+++ kmp-post.sh
@@ -24,6 +24,27 @@ depmod %2-$flavor
 
 %{_sbindir}/update-alternatives --install /usr/lib/nvidia/alternate-install-present alternate-install-present /usr/lib/nvidia/alternate-install-present-$flavor 11
 
+# Create symlinks for udev so these devices will get user ACLs by logind later (bnc#1000625)
+ln -snf /dev/nvidiactl /run/udev/static_node-tags/uaccess/nvidiactl 
+ln -snf /dev/nvidia-uvm /run/udev/static_node-tags/uaccess/nvidia-uvm
+cat >  /etc/tmpfiles.d/nvidia-hack.conf << EOF
+L /run/udev/static_node-tags/uaccess/nvidiactl - - - - /dev/nvidiactl
+L /run/udev/static_node-tags/uaccess/nvidia-uvm - - - - /dev/nvidia-uvm
+EOF
+devid=-1
+for dev in $(ls -d /sys/bus/pci/devices/*); do 
+  vendorid=$(cat $dev/vendor)
+  if [ "$vendorid" == "0x10de" ]; then 
+    class=$(cat $dev/class)
+    classid=${class%%00}
+    if [ "$classid" == "0x0300" -o "$classid" == "0x0302" ]; then 
+      devid=$((devid+1))
+      ln -snf /dev/nvidia${devid} /run/udev/static_node-tags/uaccess/nvidia${devid}
+      echo "L /run/udev/static_node-tags/uaccess/nvidia${devid} - - - - /dev/nvidia${devid}" >> /etc/tmpfiles.d/nvidia-hack.conf
+    fi
+  fi
+done
+
 echo
 echo "Modprobe blacklist files have been created at /etc/modprobe.d to \
 prevent Nouveau from loading. This can be reverted by deleting \
