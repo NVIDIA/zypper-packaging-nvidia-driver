4fb32ad43132ce01a3f0089ef406c60bfd68aed6
Stefan Dirsch <sndirsch@suse.com>
2018-10-02 06:10:00 -0700

    - adjusted generate-service-file.sh - generated _service file

--- generate-service-file.sh
+++ generate-service-file.sh
@@ -2,7 +2,7 @@
 
 driver_version=$(grep -i ^version: nvidia-gfxG0?.spec |awk '{print $2}')
 
-for arch in x86 x86_64; do
+for arch in x86_64; do
   file=NVIDIA-Linux-${arch}-${driver_version}.run
   if [ ! -s ${file} ]; then
     echo -n "Dowloading ${file} ... "
@@ -11,7 +11,7 @@ for arch in x86 x86_64; do
   fi
 done
 
-for arch in x86 x86_64; do
+for arch in x86_64; do
   file=NVIDIA-Linux-${arch}-${driver_version}.run
   test -f ${file}
   if [ $? -ne 0 ]; then
@@ -34,18 +34,12 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-sha256sum_x86=$(sha256sum NVIDIA-Linux-x86-${driver_version}.run | awk '{print $1}')
 sha256sum_x86_64=$(sha256sum NVIDIA-Linux-x86_64-${driver_version}.run | awk '{print $1}')
 
 echo -n "Creating _service file ..."
 cat << EOF > _service
 <services>
   <service name="download_files" mode="disabled"/>
-  <service name="verify_file" mode="disabled">
-    <param name="file">NVIDIA-Linux-x86-${driver_version}.run</param>
-    <param name="verifier">sha256</param>
-    <param name="checksum">${sha256sum_x86}</param>
-  </service>
   <service name="verify_file" mode="disabled">
     <param name="file">NVIDIA-Linux-x86_64-${driver_version}.run</param>
     <param name="verifier">sha256</param>
