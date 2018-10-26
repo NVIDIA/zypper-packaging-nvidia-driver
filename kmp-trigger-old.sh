8c6c6b683003fc54e40114d4b9c24758b24caffe
Stefan Dirsch <sndirsch@suse.com>
2018-10-26 05:53:39 -0700

    redirect sterr of find in trigger script to /dev/null

--- kmp-trigger-old.sh
+++ kmp-trigger-old.sh
@@ -1,6 +1,6 @@
 # get rid of broken weak-updates symlinks created in some %post apparently;
 # either by kmp itself or by kernel package update
-for i in $(find /lib/modules/*/weak-updates -type l); do
+for i in $(find /lib/modules/*/weak-updates -type l 2> /dev/null); do
   test -e $(readlink -f $i) || rm $i
 done
 %ifarch %ix86
