823286fc1fdc35cc8e679ccaf9fbd6c07709cd99
Andy Dick <adick@nvidia.com>
2022-05-31 19:23:07 +0000

    Only remove .ko files on uninstall, not upgrade.
    Removing files on upgrade actually causes the upgraded .ko files to be removed since %postun of the old package runs AFTER the %post of the new package.

--- kmp-postun.sh
+++ kmp-postun.sh
@@ -6,8 +6,8 @@ if [ "$1" = 0 ] ; then
     fi
     # cleanup of bnc# 1000625
     rm -f /usr/lib/tmpfiles.d/nvidia-logind-acl-trick-G05.conf
-fi
 
-# Remove old .ko files that the %ghost didn't clean up
-# Runs regardless of upgrade or uninstall ($1 = 0 or $1 = 1)
-rm /lib/modules/*/updates/nvidia*.ko
+    # Remove old .ko files that the %ghost didn't clean up
+    # Runs ONLY during uninstall due to ordering (running during upgrade would remove the just-installed .ko files)
+    rm /lib/modules/*/updates/nvidia*.ko
+fi
